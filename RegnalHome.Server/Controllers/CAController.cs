9424b11cb0f633a48371609004a3a8167d684ed3
Langerovi-2\Langerovi <pavel.lgr@seznam.cz>
2023-07-11 23:07:26 +0200

    .

---
+++ RegnalHome.Server/Controllers/CAController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RegnalHome.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CAController : ControllerBase
+    {
+        [HttpGet]
+        public FileContentResult Get()
+        {
+            var myfile = System.IO.File.ReadAllBytes("regnalCA.cer");
+            return new FileContentResult(myfile, "application/x-x509-user-cert");
+        }
+    }
+}
